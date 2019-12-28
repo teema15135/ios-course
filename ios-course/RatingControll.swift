d8c7792577346d1d4eaac4d908b27b12ff0310f0
Teema Luksameset <tm@odds.team>
2019-12-28 16:50:09 +0700

    end course

--- ios-course/RatingControll.swift
+++ ios-course/RatingControll.swift
@@ -14,18 +14,20 @@ class RatingControll: UIStackView {
     @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
         didSet {
             setupButtons()
+            updateButtonSelectionState()
         }
     }
     @IBInspectable var starCount: Int = 5 {
         didSet {
             setupButtons()
+            updateButtonSelectionState()
         }
     }
     
-    private var ratingButtons: [UIButton] = []
-    
     var rating = 0
     
+    private var ratingButtons: [UIButton] = []
+
     // MARK: Initialization
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -54,6 +56,7 @@ class RatingControll: UIStackView {
         let bundle = Bundle(for: type(of: self))
         let yellowStar = UIImage(named: "yellowStar", in: bundle, compatibleWith: self.traitCollection)
         let blackStar = UIImage(named: "blackStar", in: bundle, compatibleWith: self.traitCollection)
+        let blueStar = UIImage(named: "blueStar", in: bundle, compatibleWith: self.traitCollection)
         
         for _ in 0 ..< starCount {
             // Create button
@@ -62,6 +65,7 @@ class RatingControll: UIStackView {
             // Set button image
             button.setImage(yellowStar, for: .selected)
             button.setImage(blackStar, for: .normal)
+            button.setImage(blueStar, for: .highlighted)
 
             // Add constraints
             button.translatesAutoresizingMaskIntoConstraints = false
@@ -78,7 +82,28 @@ class RatingControll: UIStackView {
         }
     }
     
+    private func updateButtonSelectionState() {
+        for (index, button) in ratingButtons.enumerated() {
+            button.isSelected = index < rating
+        }
+    }
+    
     @objc func ratingButtonTapped(button: UIButton) {
+        guard let index = ratingButtons.firstIndex(of: button) else {
+            fatalError("The button, \(button), is not in the ratingButtons array: \(ratingButtons)")
+        }
+        
+        // Calculate the rating of the selected button
+        let selectedRating = index + 1
+        
+        if selectedRating == rating {
+            rating = 0
+        } else {
+            rating = selectedRating
+        }
+        
+        updateButtonSelectionState()
+        
         print("Button pressed 👌🏻")
     }
 }
